a0011b14b1fca02ea56e0445b7ebcce93af505b9
Angad-Godara <angadrrr2002@gmail.com>
2023-04-22 10:59:58 +0530

    ADD: added firebase in frontend

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCu1ei4KeDp0tKPnBeI4z1YuNUc7qZvQBY",
+    authDomain: "spotify-clone-8e736.firebaseapp.com",
+    projectId: "spotify-clone-8e736",
+    storageBucket: "spotify-clone-8e736.appspot.com",
+    messagingSenderId: "143538403328",
+    appId: "1:143538403328:web:a8dfbe31ae04eee0ab40e5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
